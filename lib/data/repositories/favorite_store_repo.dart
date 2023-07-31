5c94758611201a3e80908513ad1634af7cac6b2c
hayolox <akiklii72@gmail.com>
2023-07-31 18:52:00 +0700

    feature fav

---
+++ lib/data/repositories/favorite_store_repo.dart
@@ -0,0 +1,38 @@
+import 'dart:convert';
+
+import 'package:fake_store_app/data/model/detail_store_model.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+import '../model/home_store_model.dart';
+
+class FavoriteStoreRepo {
+  Future<void> addFavorite(DetailStoreModel dataStoreModel) async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    List<String>? items = prefs.getStringList('items');
+
+    items ??= [];
+
+    if (!items.contains(jsonEncode(dataStoreModel.toJson()))) {
+      items.add(jsonEncode(dataStoreModel.toJson()));
+
+      await prefs.setStringList('items', items);
+    }
+  }
+
+  Future<List<HomeStoreModel>> getFavorites() async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    List<String>? items = prefs.getStringList('items');
+
+    if (items == null) {
+      return [];
+    }
+
+    List<HomeStoreModel> favoriteStores = [];
+    for (String item in items) {
+      Map<String, dynamic> json = jsonDecode(item);
+      favoriteStores.add(HomeStoreModel.fromJson(json));
+    }
+
+    return favoriteStores;
+  }
+}
